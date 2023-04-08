8fc035162a24f7aa6acc611b2dbf9a49760fb490
jdmlopes <jdlopes97@gmail.com>
2023-04-08 12:51:45 -0300

    Implement checkItem model module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-//TODO
+import checkItemFactory from "./models/checkItem.js";
+
+let test = checkItemFactory("test", true);
+
+console.log(test);
--- src/models/checkItem.js
+++ src/models/checkItem.js
@@ -1,1 +1,22 @@
-//TODO
+export default function checkItemFactory(title, done) {
+  let _title = title;
+  let _done = done;
+
+  const toggleDone = () => {
+    _done = _done ? false : true;
+  };
+  return {
+    get title() {
+      return _title;
+    },
+    set title(title) {
+      _title = title;
+    },
+    get done() {
+      return _done;
+    },
+    set done(done) {
+      _done = done;
+    },
+  };
+}
