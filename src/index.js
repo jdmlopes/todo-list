582009a9d451c4c48a4027af664c11fcd87f3d6f
jdmlopes <jdlopes97@gmail.com>
2023-04-09 07:42:01 -0300

    Implement task factory module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import checkItemFactory from "./models/checkItem.js";
+import taskFactory from "./models/task.js";
 
-let test = checkItemFactory("test", true);
+let s_task = checkItemFactory("test", true);
+let s_task2 = checkItemFactory("a sub task", false);
+let task = taskFactory(
+  "a simple task",
+  false,
+  "high",
+  "a task i have to do",
+  new Date(),
+  [s_task, s_task2],
+  "none"
+);
 
-console.log(test);
+console.log(task);
--- src/models/task.js
+++ src/models/task.js
@@ -1,1 +1,62 @@
-//TODO
+export default function taskFactory(
+  title,
+  done,
+  priority,
+  description,
+  dueDate,
+  checkList,
+  categoryName
+) {
+  let _title = title;
+  let _done = done;
+  let _priority = priority;
+  let _description = description;
+  let _dueDate = dueDate;
+  let _checkList = checkList;
+  let _categoryName = categoryName;
+
+  return {
+    get title() {
+      return _title;
+    },
+    get done() {
+      return _done;
+    },
+    get priority() {
+      return _priority;
+    },
+    get description() {
+      return _description;
+    },
+    get dueDate() {
+      return _dueDate;
+    },
+    get checkList() {
+      return _checkList;
+    },
+    get categoryName() {
+      return _categoryName;
+    },
+    set title(title) {
+      _title = title;
+    },
+    set done(done) {
+      _done = done;
+    },
+    set priority(priority) {
+      _priority = priority;
+    },
+    set description(description) {
+      _description = description;
+    },
+    set dueDate(dueDate) {
+      _dueDate = dueDate;
+    },
+    set checkList(checkList) {
+      _checkList = checkList;
+    },
+    set categoryName(categoryName) {
+      _categoryName = categoryName;
+    },
+  };
+}
